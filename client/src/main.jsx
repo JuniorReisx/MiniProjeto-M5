e05a9af1123dfe34c9926686b61362fa1b5b9ade
Junior Reis <gilmaikjunior@gmail.com>
2024-09-07 20:25:44 -0300

    🔧 chore: update project structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Page from './pages/Page.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Page/>
   </StrictMode>,
 )
---
+++ client/src/pages/Page.jsx
@@ -0,0 +1,15 @@
+import './Page.module.css'
+import Header from '../components/Header/Header.jsx'
+
+function Page() {
+
+
+  return (
+    <>
+    <Header/>
+
+    </>
+  )
+}
+
+export default Page
