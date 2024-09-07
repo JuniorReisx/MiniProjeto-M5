5b1b3e8ccc07400cfbc2e750e2c058caea12b47c
Junior Reis <gilmaikjunior@gmail.com>
2024-09-07 20:09:45 -0300

    🎨 chore: add new header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.module.css'
+import { FaUserDoctor } from "react-icons/fa6";
+
+export default function Header(){
+
+    return(
+        <>
+        
+        <header>
+            <h1>
+            Hospital System
+            </h1>
+          <nav>
+            
+
+            <FaUserDoctor size={25} style={{ cursor: 'pointer' }} />
+
+          </nav>
+
+        </header>
+
+        </>
+    )
+
+}
