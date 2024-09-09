01da4168811b6e3402b67aaafad538b8549fc2d5
Junior Reis <gilmaikjunior@gmail.com>
2024-09-09 18:06:35 -0300

    ✨ feat: create patient route

--- server/src/routes/patient.routes.js
+++ server/src/routes/patient.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import { getPatient } from "../controllers/patient/getPatient.js"
+import { createPatient } from "../controllers/patient/createPatient.js"
 const patientRouter = Router()
 
 patientRouter.get("/patient/all", getPatient)
+patientRouter.post("/patient", createPatient)
+
 
 
 export { patientRouter }
