1523c5cce40e81e41d1a74a1c0b4e55de98fde21
Junior Reis <gilmaikjunior@gmail.com>
2024-09-22 16:27:14 -0300

    🚀 deploy: upload header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,16 @@
-import './Header.module.css'
+import "./Header.css";
+import "../../index.css";
 import { FaUserDoctor } from "react-icons/fa6";
 
-export default function Header(){
-
-    return(
-        <>
-        
-        <header>
-            <h1>
-            Hospital System
-            </h1>
-          <nav>
-            
-
-            <FaUserDoctor size={25} style={{ cursor: 'pointer' }} />
-
-          </nav>
-
-        </header>
-
-        </>
-    )
-
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>Hospital System</h1>
+        <nav>
+          <FaUserDoctor size={25} style={{ cursor: "pointer" }} />
+        </nav>
+      </header>
+    </>
+  );
 }
