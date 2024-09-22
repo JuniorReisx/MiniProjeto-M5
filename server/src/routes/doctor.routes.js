d848d84862c5a91a4646b6965783b62fff81d688
Junior Reis <gilmaikjunior@gmail.com>
2024-09-22 16:18:28 -0300

    🐛 chore: delete unused files

--- server/src/routes/doctor.routes.js
+++
@@ -1,9 +0,0 @@
-// import { Router } from "express";
-// const doctorRouter = Router()
-
-// doctorRouter.get("/doctor/all", )
-// doctorRouter.post("/doctor", )
-// doctorRouter.delete("/doctor/delete/:id", )
-// doctorRouter.put("/doctor/update/:id", )
-
-// export { doctorRouter }
