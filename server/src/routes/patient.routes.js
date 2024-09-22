4a010a0b695dbb28788010f5bdd6f259b8c53396
Junior Reis <gilmaikjunior@gmail.com>
2024-09-22 16:23:27 -0300

    🚀 deploy: upload patient routes

--- server/src/routes/patient.routes.js
+++ server/src/routes/patient.routes.js
@@ -1,16 +1,17 @@
-import { Router } from "express"
-import { getPatient } from "../controllers/patient/getPatient.js"
-import { createPatient } from "../controllers/patient/createPatient.js"
-import { deletePatient } from "../controllers/patient/deletePatient.js"
-import { updatePatient } from "../controllers/patient/updatePatient.js"
-const patientRouter = Router()
+import { Router } from "express";
+import { getPatient } from "../controllers/patient/getPatient.js";
+import { createPatient } from "../controllers/patient/createPatient.js";
+import { deletePatient } from "../controllers/patient/deletePatient.js";
+import { updatePatient } from "../controllers/patient/updatePatient.js";
+import { getPatientByCPF } from "../controllers/patient/getOnePatient.js";
+import { getGenderPatient } from "../controllers/patient/getGenderPatient.js";
+const patientRouter = Router();
 
-patientRouter.get("/patient/all", getPatient)
-patientRouter.post("/patient", createPatient)
-patientRouter.delete("/patient/delete/:codePatient", deletePatient)
-patientRouter.put("/patient/update/:codePatient", updatePatient)
+patientRouter.get("/patient/all", getPatient);
+patientRouter.get("/patient/all/gender/:gender", getGenderPatient);
+patientRouter.get("/patient/getPatientByCPF/:cpf", getPatientByCPF);
+patientRouter.post("/patient", createPatient);
+patientRouter.delete("/patient/delete/:codePatient", deletePatient);
+patientRouter.put("/patient/update/:codePatient", updatePatient);
 
-
-
-
-export { patientRouter }
+export { patientRouter };
