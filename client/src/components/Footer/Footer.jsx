eab19b7578ba19e28f3b56f248947a6f8d9df790
Junior Reis <gilmaikjunior@gmail.com>
2024-09-22 16:26:17 -0300

    ✨ feat: create footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+import "../../index.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>
+          <strong>Feito por:</strong> Gil Maik
+        </p>
+      </footer>
+    </>
+  );
+}
